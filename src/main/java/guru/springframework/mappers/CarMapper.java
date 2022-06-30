525c58dbc974d75e0befa3d280fa01a4d86c6669
Alex <azakotyansky@gmail.com>
2022-06-29 22:34:43 -0400

    update mapStruct version

--- src/main/java/guru/springframework/mappers/CarMapper.java
+++ src/main/java/guru/springframework/mappers/CarMapper.java
@@ -7,6 +7,7 @@ import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 /**
+ * Write this mapper
  * Define this interface to convert for / to both ways
  * mapstruct plugin goal will generate the class to convert.
  * Click on implementayion, line 15
