acacf9411fbc6d5e6194bf15f72bc3936280392f
Alex <azakotyansky@gmail.com>
2022-06-29 22:39:23 -0400

    update mapStruct version

--- src/main/java/guru/springframework/mappers/UserMapper.java
+++ src/main/java/guru/springframework/mappers/UserMapper.java
@@ -7,6 +7,12 @@ import org.mapstruct.factory.Mappers;
 
 /**
  * Write this mapper
+ * 1. Define API to convert a User to UserCommand
+ * 2. Define API to convert a UserCommand to User
+ *
+ * That is only what we need for mapstruct. Now, use mvn to package on that and
+ * check generated implementation in line 26
+ *
  * For different field names, we will need to add @Mappings annotation
  * see https://www.baeldung.com/mapstruct
  *
@@ -20,7 +26,13 @@ import org.mapstruct.factory.Mappers;
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    /**
+     * Define API to convert a User to UserCommand
+     */
     UserCommand userToUserCommand(User user);
 
+    /**
+     * Define API to convert a UserCommand to User
+     */
     User userCommandToUser(UserCommand userCommand);
 }
